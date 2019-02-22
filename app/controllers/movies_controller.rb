922d9f00556e4334cc55bb79ed10f8a4a5c7b675
EC2 Default User <ec2-user@ip-172-31-21-117.us-east-2.compute.internal>
2019-02-22 01:18:26 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,19 +37,21 @@ class MoviesController < ApplicationController
     if (params[:sort_using].present?)
       session[:sort_using] = params[:sort_using] #remember if sorting settings explicitly specified
     else
-      @flag3 = 1 #assign flag value if no sorting paramenters present
+      #@flag3 = 1 #assign flag value if no sorting paramenters present
       params[:sort_using] = session[:sort_using] #assign stored sorting settings to current settings
+      redirect_to movies_path(params) and return
     end
     if (params[:ratings].present?) 
       session[:ratings] = params[:ratings] #remember if filter settings explicitly specified
     else
-      @flag4 = 1 #assign flag value if no sorting paramenters present
+      #@flag4 = 1 #assign flag value if no sorting paramenters present
       params[:ratings] = session[:ratings] #assign stored filter settings to current settings
+      redirect_to movies_path(params)
     end
-    if @flag3 && @flag4 # If incoming URI is lacking the right params[] and you're forced to fill them in from the session[], the RESTful thing to do is to redirect_to the new URI containing the appropriate parameters
+    #if @flag3 && @flag4 # If incoming URI is lacking the right params[] and you're forced to fill them in from the session[], the RESTful thing to do is to redirect_to the new URI containing the appropriate parameters
       #flash.keep # since the flash[] only survives across a single redirect
       #redirect_to movies_path(params) and return
-    end
+    #end
     if session[:ratings].present?
       @checked = session[:ratings].keys
     end
