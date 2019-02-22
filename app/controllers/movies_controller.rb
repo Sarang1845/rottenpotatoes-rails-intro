f9047e177f306bea6c3f5a05394bca2a93eb025f
EC2 Default User <ec2-user@ip-172-31-21-117.us-east-2.compute.internal>
2019-02-22 01:34:36 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,54 +11,24 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
-    #@all_ratings = Movie.allratings
-    #if params[:ratings].present?
-    #  temp = params[:ratings]
-    #  @checked = temp.keys
-    #  if @checked.present?
-    #    @movies = Movie.all.where(rating: @checked)
-    #    
-    #  else
-    #    @movies = Movie.all
-    #  end
-    #end
-    #if params[:sort_using] == 'title'
-    #  @movies = @movies.order(:title)
-    #  @flag1 = 'hilite'
-    #elsif params[:sort_using] == 'release_date'
-    #  @movies = @movies.order(:release_date)
-    #  @flag2 = 'hilite'
-    #else  
-    #  @movies = @movies.all
-    #end
     @movies = Movie.all
     @all_ratings = Movie.allratings
     if (params[:sort_using].present?)
       session[:sort_using] = params[:sort_using] #remember if sorting settings explicitly specified
     else
-      #@flag3 = 1 #assign flag value if no sorting paramenters present
       params[:sort_using] = session[:sort_using] #assign stored sorting settings to current settings
-     # redirect_to movies_path(params) and return
     end
     if (params[:ratings].present?) 
       session[:ratings] = params[:ratings] #remember if filter settings explicitly specified
     else
-      #@flag4 = 1 #assign flag value if no sorting paramenters present
       params[:ratings] = session[:ratings] #assign stored filter settings to current settings
-     # redirect_to movies_path(params) and return
     end
-    #if @flag3 && @flag4 # If incoming URI is lacking the right params[] and you're forced to fill them in from the session[], the RESTful thing to do is to redirect_to the new URI containing the appropriate parameters
-      #flash.keep # since the flash[] only survives across a single redirect
-      #redirect_to movies_path(params) and return
-    #end
+    
     if session[:ratings].present?
       @checked = session[:ratings].keys
     end
     if @checked.present?
       @movies = Movie.all.where(rating: @checked)
-    #else
-    #  @movies = Movie.all
     end
     if session[:sort_using] == 'title'
       @movies = @movies.order(:title)
