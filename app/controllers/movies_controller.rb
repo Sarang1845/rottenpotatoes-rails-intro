4560cee8b990b66244233db9044b16960ff226a9
Sarang1845 <47195304+Sarang1845@users.noreply.github.com>
2019-02-21 20:11:08 -0600

    Add files via upload

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,31 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.allratings
+    if (params[:sort_using].present?)
+      session[:sort_using] = params[:sort_using] #remember if sorting settings explicitly specified
+    else
+      params[:sort_using] = session[:sort_using] #assign stored sorting settings to current settings
+    end
+    if (params[:ratings].present?) 
+      session[:ratings] = params[:ratings] #remember if filter settings explicitly specified
+    else
+      params[:ratings] = session[:ratings] #assign stored filter settings to current settings
+    end
+    
+    if session[:ratings].present?
+      @checked = session[:ratings].keys
+    end
+    if @checked.present?
+      @movies = Movie.all.where(rating: @checked)
+    end
+    if session[:sort_using] == 'title'
+      @movies = @movies.order(:title)
+      @flag1 = 'hilite' # set the CSS class of the 'Movie Title' heading to hilite only if the Movie Title link is clicked
+    elsif session[:sort_using] == 'release_date'
+      @movies = @movies.order(:release_date) # set the CSS class of the 'Release Date' heading to hilite only if the 	Release Date heading link is clicked
+      @flag2 = 'hilite'
+    end  
   end
 
   def new
